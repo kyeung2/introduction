326640f76c9e1d9fa24d8098344f2f2019e52265
Kye Yeung <yeung.kye@pm.me>
2020-04-03 15:09:52 +0100

    able to call the Lambda with the creds, pretty cool

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,13 @@ import LoginForm from "./LoginForm"
 import ResendForm from "./ResendForm";
 import {userPool} from "./UserUtils";
 import * as AWS from 'aws-sdk/global';
+import {Lambda} from "aws-sdk";
 
 function App() {
 
     function loadAuthenticatedUser() {
         console.log("load authenticated user");
-        var cognitoUser = userPool.getCurrentUser();
+        const cognitoUser = userPool.getCurrentUser();
         if (cognitoUser != null) {
             cognitoUser.getSession(function (err, session) {
                 if (err) {
@@ -53,11 +54,30 @@ function App() {
                     } else {
                         console.log('refreshed creds');
                         console.log(AWS.config.credentials);
+
+
+                        // invoke Lambda directly, API Gateway next. Pretty cool it's able to do this already,
+                        // pretty smooth sailing to be honest.
+                        const lambda = new Lambda({
+                            credentials: AWS.config.credentials,
+                            region: "eu-west-1"
+                        });
+
+                        const params = {
+                            FunctionName: 'introduction',
+                            InvocationType: 'RequestResponse'
+                        };
+                        lambda.invoke(params, (error, result) => {
+                            if (error) {
+                                console.error(error);
+                            } else {
+                                const payload = JSON.parse(result.Payload);
+                                const body = JSON.parse(payload.body);
+                                console.log(body)
+                            }
+                        })
                     }
                 });
-
-                // Instantiate aws sdk service objects now that the credentials have been updated.
-                // example: var s3 = new AWS.S3();
             });
         } else {
             document.title = `Flying Nimbus`
